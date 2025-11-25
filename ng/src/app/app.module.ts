91b0bf5bf06930060b2937f37e13b52dbf489ada
Paul Pietzko <paul.pietzko@icloud.com>
2025-11-25 10:33:56 +0100

    feat: update to angular 18

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { ContentManagerModule, SxcRootModule } from "@2sic.com/sxc-angular";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { NgModule, Provider } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 
 const providers: Provider[] = [];
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    AppRoutingModule,
-    SxcRootModule,
-    ContentManagerModule,
-
-    BrowserAnimationsModule,
-    HttpClientModule,
-  ],
-  providers,
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [AppRoutingModule,
+        SxcRootModule,
+        ContentManagerModule,
+        BrowserAnimationsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
