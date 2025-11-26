ace22468beb00f0c0d40e81e7f7ea67a567290a4
Paul Pietzko <paul.pietzko@icloud.com>
2025-11-26 09:52:50 +0100

    refactor: filter-checkboxes signal wip

--- ng/src/app/filter-options/filter-checkboxes/filter-checkboxes.component.ts
+++ ng/src/app/filter-options/filter-checkboxes/filter-checkboxes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, inject, input, effect, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 import {
   FilterCategoryGroup,
   FilterOption,
@@ -11,11 +11,10 @@ import {
   ReactiveFormsModule,
 } from "@angular/forms";
 import { FilterOptionsService } from "../fiter-options.services";
-import { map } from "rxjs/operators";
 import { CheckboxIds } from "../filter-options.enums";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { MatCheckboxModule } from "@angular/material/checkbox";
-import { AsyncPipe, JsonPipe } from "@angular/common";
+import { JsonPipe } from "@angular/common";
 
 @Component({
   selector: "app-filter-checkboxes",
@@ -26,75 +25,70 @@ import { AsyncPipe, JsonPipe } from "@angular/common";
     ReactiveFormsModule,
     MatCheckboxModule,
     MatTooltipModule,
-    AsyncPipe,
     JsonPipe,
   ],
 })
-export class FilterCheckboxesComponent implements OnInit {
-  @Input() titlePrefix: string = null;
-  @Input() checkboxGroups$: Observable<FilterCategoryGroup[]> = new Observable<
-    FilterCategoryGroup[]
-  >();
-  // Definition of the form controlling the checkboxes
+export class FilterCheckboxesComponent implements OnDestroy {
+  filterService = inject(FilterOptionsService);
+
+  checkboxGroupsSig = input.required<FilterCategoryGroup[]>();
+
+  // FormGroup controlling the checkboxes
   public checkboxForm: FormGroup = new FormGroup({});
+  private subscription = new Subscription();
 
-  constructor(public filterService: FilterOptionsService) {}
+  constructor() {
+    // Effekt reagiert automatisch auf Änderungen von checkboxGroupsSig
+    effect(() => {
+      const groups = this.checkboxGroupsSig();
+      if (!groups || groups.length === 0) return;
 
-  ngOnInit() {
-    // Fetch the checkbox groups and their options
-    this.checkboxGroups$
-      .pipe(
-        // First transformation: Arrange options in the desired order
-        map((groups: FilterCategoryGroup[]) =>
-          groups.map((group: FilterCategoryGroup) => {
-            const checkboxOrderIds = [
-              CheckboxIds.stabel,
-              CheckboxIds.tutorial,
-              CheckboxIds.featueDemo,
-              CheckboxIds.old,
-            ];
-            const orderedOptions = checkboxOrderIds
-              .map((id: number) =>
-                group.Options.find((option: FilterOption) => option.Id === id)
-              )
-              .filter(Boolean) as FilterOption[]; // drop any missing entries
-            group.Options = orderedOptions;
-            return group;
-          })
-        ),
-        // Second transformation: Adjust tags and tooltips of options
-        map((groups: FilterCategoryGroup[]) =>
-          groups.map((group: FilterCategoryGroup) => {
-            group.Options.map((option: FilterOption) => {
-              if (option.Id === CheckboxIds.old) {
-                option.Tag = "Hide all old Apps";
-              }
+      console.log("Checkbox Groups Signal updated:", groups);
 
-              if (option.Id === CheckboxIds.stabel) {
-                option.Tooltip = option.Title;
-                option.Tag = "Stable";
-              }
+      // Transformationen: Reihenfolge, Tags, Tooltips
+      groups.forEach((group: FilterCategoryGroup) => {
+        const checkboxOrderIds = [
+          CheckboxIds.stabel,
+          CheckboxIds.tutorial,
+          CheckboxIds.featueDemo,
+          CheckboxIds.old,
+        ];
 
-              return option;
-            });
-            return group;
-          })
-        )
-      )
-      .subscribe((groups: FilterCategoryGroup[]) => {
-        // Add checkboxes to the form
-        groups.forEach((group: FilterCategoryGroup) =>
-          group.Options.forEach((option: FilterOption) => {
-            // Set 'old' checkbox to true
-            // const isOldCheckbox = option.Id === CheckboxIds.old;
-            this.checkboxForm.addControl(
-              option.Title,
-              new FormControl(!option.ShowApps)
-              // new FormControl(isOldCheckbox ? true : !option.ShowApps) // Setzt 'old' von Anfang an auf true
+        // Sort options
+        group.Options = checkboxOrderIds
+          .map((id: number) => group.Options.find((opt) => opt.Id === id))
+          .filter(Boolean) as FilterOption[];
+
+        // Update tags/tooltips
+        group.Options.forEach((option) => {
+          if (option.Id === CheckboxIds.old) option.Tag = "Hide all old Apps";
+          if (option.Id === CheckboxIds.stabel) {
+            option.Tooltip = option.Title;
+            option.Tag = "Stable";
+          }
+
+          // Add FormControl if it does not exist
+          if (!this.checkboxForm.get(option.Title)) {
+            const control = new FormControl(!option.ShowApps);
+            this.checkboxForm.addControl(option.Title, control);
+
+            this.subscription.add(
+              control.valueChanges.subscribe((state: boolean) => {
+                if (state) {
+                  this.filterService.setFilter(option);
+                } else {
+                  this.filterService.removeFilter(option);
+                }
+              })
             );
-          })
-        );
+          }
+        });
       });
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   // Check if some checkboxes are selected
